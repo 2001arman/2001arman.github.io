f739f73ec28834fe488bb4742aa46ddea5e6354d
Arman <trisaribaru@gmail.com>
2021-07-28 20:56:08 +0800

    project web arman

---
+++ navbar.php
@@ -0,0 +1,29 @@
+<nav class="navbar navbar-expand-lg navbar-dark fixed-top">
+      <div class="container">
+        <a class="navbar-brand" href="index.html">INDEHOY</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarScrollingDropdown" role="button" data-toggle="dropdown" aria-expanded="false">Tutorial</a>
+              <ul class="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
+                <li><a class="drowpdown-item" href="#">Android</a></li>
+                <li><a class="drowpdown-item" href="#">Komputer</a></li>
+              </ul>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarScrollingDropdown" role="button" data-toggle="dropdown" aria-expanded="false">Pemrograman</a>
+              <ul class="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
+                <li><a class="drowpdown-item" href="#">C++</a></li>
+                <li><a class="drowpdown-item" href="#">Python</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+</nav>
